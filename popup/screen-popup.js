b6bec82df4e69cdb5dc1f69aca066153d8643f2d
mhubenthal <hubentmx@gmail.com>
2015-04-10 16:50:50 -0400

    add in hello world code

--- popup/screen-popup.js
+++ popup/screen-popup.js
@@ -1,6 +1,7 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	chrome.tabs.sendMessage(tabs[0].id, {greeting: 'hello'}, function(response){
-		console.log(response.farewell);
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {greeting: 'hello'}, function(response){
+			console.log(response.farewell);
+		});
 	});
-});
 
+function draw() { alert('test');}
