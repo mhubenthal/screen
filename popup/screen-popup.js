7cc0d25959324418b7d1b8fe84a8cb397f5358d2
mhubenthal <hubentmx@gmail.com>
2015-04-11 21:21:21 -0400

    experiment with extension code

--- popup/screen-popup.js
+++ popup/screen-popup.js
@@ -1,7 +1,33 @@
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {greeting: 'hello'}, function(response){
-			console.log(response.farewell);
-		});
-	});
+/*function click(e) {
+  chrome.tabs.executeScript(null,
+      //{code:"document.body.style.backgroundColor='" + e.target.id + "'"}
+{code: 'addElement()'}
+);
+  window.close();
+}
+*/
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', addElement);
+  }
+});
 
-function draw() { alert('test');}
+function addElement (bd) {
+  // create a new div element 
+  // and give it some content 
+  var newDiv = document.createElement("div");
+	newDiv.style.zIndex = '1000';
+	newDiv.style.border = '1px solid red';
+//	bd.appendChild(newDiv);	
+}	
+
+chrome.runtime.getBackgroundPage(function(backgroundPage){
+//	backgroundPage.alert('hello');
+	console.log(backgroundPage.document.body);
+  var newDiv = backgroundPage.document.createElement("div");
+	newDiv.style.zIndex = '1000';
+	newDiv.style.border = '1px solid red';
+	backgroundPage.document.body.appendChild(newDiv);	
+});
+
