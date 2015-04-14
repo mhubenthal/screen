bd3cf6c3cc64d4889af84ff1202ebe02e95f58f9
mhubenthal <hubentmx@gmail.com>
2015-04-13 22:05:36 -0400

    testing extension

--- js/screen-background.js
+++ js/screen-background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.setBadgeText({text: "scrn"});
+/*chrome.browserAction.setBadgeText({text: "scrn"});
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	switch(request.type) {
@@ -13,8 +13,21 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 chrome.runtime.onConnect.addListener(function(port) {
 	if(port.name === "screen-channel") {
 		port.onMessage.addListener(function(msg) {
-			alert("2. received message: "+msg+", from content script");
-			port.postMessage({myProperty: "3. thanks for the message background js"});
+			alert("2. received message: "+msg.myProperty+", from content script");
+//			port.postMessage({myProperty: "3. thanks for the message background js"});
 		});
 	}
-}); 
+});*/
+
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
+});}
--- js/screen-content.js
+++ js/screen-content.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+/*window.addEventListener("load", function() {
 	chrome.extension.sendMessage({
 		type: "dom-loaded",
 		data: {
@@ -9,7 +9,15 @@ window.addEventListener("load", function() {
 
 // Open up communication channel between page and extension
 var port = chrome.runtime.connect({name: "screen-channel"});
-port.postMessage({myProperty: "testing out screen"});
+//port.postMessage({myProperty: "testing out screen"});
 port.onMessage.addListener(function(msg) {
-	alert("1. from background-script: " + msg);	
+	alert("1. from background-script: " + msg.myProperty);	
 });
+*/
+document.onreadystatechange = function(){
+if(document.readyState === 'complete') {
+document.body.style.backgroundColor = 'red';
+
+console.log(window.document.body.style.backgroundColor);
+}
+};
