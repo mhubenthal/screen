48136ad51af0135c680df2e369dadb086ce3f3f9
mhubenthal <hubentmx@gmail.com>
2015-04-15 22:24:55 -0400

    add canvas grid

--- js/screen-content.js
+++ js/screen-content.js
@@ -25,26 +25,35 @@ document.body.onload = makeScreenCanvas();
 
 // Create screen canvas object, return canvas context for drawing to
 function makeScreenCanvas() {
+	var container = document.createElement('div');
+	container.style.pointerEvents = 'none';
 	var screenCanvas = document.createElement('canvas');
 	screenCanvas.id = 'screen';
 	var currentWidth = window.innerWidth;
 	var currentHeight = window.innerHeight;
-	screenCanvas.style.width = currentWidth + 'px';
-	screenCanvas.style.height = currentHeight + 'px';
+	screenCanvas.width = currentWidth;
+	screenCanvas.height = currentHeight;
 	screenCanvas.style.position = 'absolute';
 	screenCanvas.style.zIndex = '10000';
 	screenCanvas.style.top = '0';
 	screenCanvas.style.left = '0';
-	document.body.appendChild(screenCanvas);
+	screenCanvas.style.pointerEvents = 'none';
+	container.appendChild(screenCanvas);
+	document.body.appendChild(container);
 	var ctx = screenCanvas.getContext('2d');
-	ctx.fillStyle = 'rgba(200,0,0,.2)';
-	//ctx.fillRect(0, 0, currentWidth, currentHeight);
-	drawLine(ctx, [10,10], [200,200]);		
+	ctx.fillStyle = 'rgba(200,0,0,.1)';
+	ctx.fillRect(0, 0, currentWidth, currentHeight);
+	for(var i = 10; i < currentWidth-10; i+=11){
+		drawLine(ctx, [i,0], [i,currentHeight]);
+		drawLine(ctx, [0,i], [currentWidth, i]);		
+	}
 }
 
 function drawLine(canvasContext, startXY, endXY) {
 	canvasContext.beginPath();
 	canvasContext.moveTo(startXY[0], startXY[1]);
 	canvasContext.lineTo(endXY[0], endXY[1]);
+	canvasContext.lineWidth = 1;
+	canvasContext.strokeStyle = '#09E4E8';
 	canvasContext.stroke();	
 }
