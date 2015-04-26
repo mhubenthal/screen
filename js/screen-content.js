9ee4ca8d340789740f802b7372304d0ea90a9c5b
mhubenthal <hubentmx@gmail.com>
2015-04-25 21:21:45 -0400

    stub out mouse events

--- js/screen-content.js
+++ js/screen-content.js
@@ -1,11 +1,36 @@
 // Listen for commands from: popup html --> background script --> content script
 chrome.runtime.onMessage.addListener(function(msg, sender, sendresponse) {
 	if (msg.command && (msg.command === 'showScreen')) {
-		makeScreenCanvas();
+		//makeScreenCanvas();
+		mouseDraw();
 	} else if (msg.command && (msg.command === 'hideScreen')) {
 		hideScreenCanvas();
 	} else {}
 });
+
+// Test out MouseEvent
+function mouseDraw(){
+	makeScreenCanvas();
+	var canvCtx = (document.getElementById('screenCanvas')).getContext('2d');
+	var currentWidth = window.document.body.offsetWidth;
+	var currentHeight = window.document.body.offsetHeight;
+	document.body.addEventListener('mousedown', function(mDownEvt) {
+		canvCtx.fillStyle = 'rgb(0,0,0)';
+		canvCtx.fillRect(mDownEvt.clientX, mDownEvt.clientY, 20, 20);
+		function mouseMoveTest(mMvEvt) {
+			canvCtx.clearRect(0,0,currentWidth,currentHeight);
+			canvCtx.fillStyle = 'rgb(0,0,0)';
+			canvCtx.fillRect(mMvEvt.clientX, mMvEvt.clientY, 20, 20);
+			mMvEvt.stopPropogation();
+		}
+		function mouseUpTest(mUpEvt) {
+			document.body.removeEventListener('mousemove', mouseMoveTest);
+			document.body.removeEventListener('mouseup', mouseUpTest);
+		}
+		document.body.addEventListener('mousemove', mouseMoveTest);
+		document.body.addEventListener('mouseup', mouseUpTest);
+	});
+}
 
 // Hide screen canvas object
 function hideScreenCanvas(releaseFlagCallback) {
@@ -32,11 +57,11 @@ function makeScreenCanvas(releaseFlagCallback) {
 	container.appendChild(screenCanvas);
 	document.body.appendChild(container);
 	var ctx = screenCanvas.getContext('2d');
-	ctx.fillStyle = 'rgba(200,0,0,.1)';
-	ctx.fillRect(0, 0, currentWidth, currentHeight);
+	//ctx.fillStyle = 'rgba(200,0,0,.1)';
+	//ctx.fillRect(0, 0, currentWidth, currentHeight);
 	for(var i = 10; i < currentWidth-10; i+=11){
-		drawLine(ctx, [i,0], [i,currentHeight]);
-		drawLine(ctx, [0,i], [currentWidth, i]);		
+		//drawLine(ctx, [i,0], [i,currentHeight]);
+		//drawLine(ctx, [0,i], [currentWidth, i]);		
 	}
 }
 
